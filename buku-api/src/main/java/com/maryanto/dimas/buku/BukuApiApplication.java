0e89f5eb4b78830cb933b168ed874fea499c90d7
dimMaryanto@win10 <software.dimmaryanto@hotmail.com>
2017-10-24 17:27:56 +0700

    membuat project spring boot

---
+++ buku-api/src/main/java/com/maryanto/dimas/buku/BukuApiApplication.java
@@ -0,0 +1,12 @@
+package com.maryanto.dimas.buku;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BukuApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BukuApiApplication.class, args);
+	}
+}
---
+++ buku-api/src/test/java/com/maryanto/dimas/buku/BukuApiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.maryanto.dimas.buku;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BukuApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
